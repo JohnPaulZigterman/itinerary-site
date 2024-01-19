9df061b213fe27b7a7d6a5a1877b0dd98766aa06
hjlee <heunjong.lee@gmail.com>
2024-01-18 20:44:11 -0500

    front end dev: app.jsx, mai..jsx configured with TestComponent.jsx

---
+++ client/src/App.jsx
@@ -0,0 +1,12 @@
+import TestComponent from './components/TestComponent';
+
+import './App.css';
+
+
+function App() {
+  return (
+    <TestComponent />
+  );
+}
+
+export default App
---
+++ client/src/components/TestComponent.jsx
@@ -0,0 +1,5 @@
+export default function TestComponent() {
+    const text = 'Anthony, Becca, John, Musa';
+  
+    return <div>Test: {text}</div>;
+}
