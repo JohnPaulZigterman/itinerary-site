77b19a4e4a70da7be209b33141f366df1b37832f
JohnPaulZigterman <johnpaulzigterman@gmail.com>
2024-01-27 18:53:31 -0500

    styled and ready for friend call

--- client/src/components/UI/User.jsx
+++ client/src/components/UI/User.jsx
@@ -1,18 +1,24 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
+import '../../styles/User.css';
+import { useMutation } from '@apollo/client';
+import { ADD_FRIEND } from '../../utils/mutations';
 
 export default function User({ username, trips }) {
     return (
 
-        <div className='trip-card'>
-            <div className='trip-info'>
-                <h3>{username}</h3>
+        <div className='user-card'>
+            <div className='user-card-header'>
+                <h3>{username}'s Profile</h3>
+                <button className='friend-button'>Add Friend</button>
             </div>
             <div className='all-trips'>
                 {trips.map((trip) => (
-                    <Link key={trip._id} to={`/trip/${trip._id}`}>
-                        <p>{trip.city}</p>
-                    </Link>
+                    <div className='trip-card'>
+                        <Link key={trip._id} to={`/trip/${trip._id}`}>
+                            <p>{trip.city}</p>
+                        </Link>
+                    </div>
                 ))}
             </div>
         </div>
