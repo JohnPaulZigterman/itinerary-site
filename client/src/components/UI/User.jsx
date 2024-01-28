67b9cd64251aa403a2ad1c000c922537f84b6a0d
JohnPaulZigterman <johnpaulzigterman@gmail.com>
2024-01-28 10:34:23 -0500

    stable version, trying to implement ADD_FRIEND mutation

--- client/src/components/UI/User.jsx
+++ client/src/components/UI/User.jsx
@@ -3,14 +3,24 @@ import { Link } from 'react-router-dom';
 import '../../styles/User.css';
 import { useMutation } from '@apollo/client';
 import { ADD_FRIEND } from '../../utils/mutations';
+import Auth from '../../utils/auth';
 
 export default function User({ username, trips }) {
+    const getProfile = Auth.getProfile();
+    let browserName;
+    if (getProfile && getProfile.data) {
+        browserName = getProfile.data.username;
+    } else {
+        console.error('User profile data is not available');
+    }
+    const [addFriend] = useMutation(ADD_FRIEND);
+
     return (
 
         <div className='user-card'>
             <div className='user-card-header'>
                 <h3>{username}'s Profile</h3>
-                <button className='friend-button'>Add Friend</button>
+                <button className='friend-button' onClick={addFriend}>Add Friend</button>
             </div>
             <div className='all-trips'>
                 {trips.map((trip) => (
