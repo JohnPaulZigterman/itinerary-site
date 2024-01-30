9c4f5d1fd6ea6a8a3b7907d44287e73130c7975f
musaomark01 <musaomark01@gmail.com>
2024-01-30 03:33:52 -0500

    show all friends alone on top of all users

--- client/src/pages/Public.jsx
+++ client/src/pages/Public.jsx
@@ -1,33 +1,45 @@
 import { useQuery } from '@apollo/client';
-import { QUERY_TRIPS_BY_CITY, QUERY_USERS } from '../utils/queries';
+import { QUERY_TRIPS_BY_CITY, QUERY_USERS, QUERY_ME } from '../utils/queries';
 import { useState } from 'react';
 import { Link } from 'react-router-dom';
 import '../styles/Public.css';
 
 export default function Public() {
-    // used useState to set the search term
     const [searchTerm, setSearchTerm] = useState('');
-    // used useState to set the input value
     const [inputValue, setInputValue] = useState('');
-    // used useQuery to get the trips by city
-    const { loading: loadingTrips, error: errorTrips, data: dataTrips } = useQuery(QUERY_TRIPS_BY_CITY, {
-        variables: { city: searchTerm },
-        skip: !searchTerm,
-    });
-    // used useQuery to get all users
-    const { loading: loadingUsers, error: errorUsers, data: dataUsers } = useQuery(QUERY_USERS);
 
-    const handleSearch = () => {
-        // set the search term to the input value
-        setSearchTerm(inputValue);
-    };
-    // if loadingTrips or loadingUsers is true, return loading
-    if (loadingTrips || loadingUsers) return <p>Loading...</p>;
-    // if errorTrips or errorUsers is true, return error
-    if (errorTrips || errorUsers) return <p>Error :(</p>;
+    // ...
+
+const { loading: loadingTrips, error: errorTrips, data: dataTrips, refetch: refetchTrips } = useQuery(QUERY_TRIPS_BY_CITY, {
+    variables: { city: searchTerm },
+    skip: !searchTerm,
+});
+
+const { loading: loadingUsers, error: errorUsers, data: dataUsers, refetch: refetchUsers } = useQuery(QUERY_USERS);
+
+const { loading: loadingMe, error: errorMe, data: dataMe, refetch: refetchMe } = useQuery(QUERY_ME);
+
+const handleSearch = () => {
+    setSearchTerm(inputValue);
+    refetchTrips(); // Refetch trips when search is triggered
+    refetchUsers(); // Refetch users when search is triggered
+    refetchMe(); // Refetch current user data when search is triggered
+};
+
+const handleUserClick = () => {
+    refetchTrips(); // Refetch trips when a user is clicked
+    refetchUsers(); // Refetch users when a user is clicked
+    refetchMe(); // Refetch current user data when a user is clicked
+};
+
+
+    if (loadingTrips || loadingUsers || loadingMe) return <p>Loading...</p>;
+    if (errorTrips || errorUsers || errorMe) return <p>Error :(</p>;
+
+    
 
     return (
-        <div className='public-page'>
+        <div className='public-page' onMouseOver={handleUserClick}>
             <div className='search-city'>
                 <h2>Title</h2>
                 <input
@@ -57,7 +69,24 @@ export default function Public() {
                 )}
             </div>
             <div className='users-thumbnails'>
-                <h1 id='users-h1'>Users</h1>
+                <h1 id='users-h1'>Friends</h1>
+                <div className='user-box'>
+                {/*  return all friends and sort them in alphabetical order */}
+                {[...dataMe.me.friends].sort((a, b) => a.username.localeCompare(b.username)).map((friend) => (
+                    // link to the friend page 
+                    <div className='user-thumbnail'>
+                        <div className='user-thumbnail-header'>
+                        <h2>{friend.username}</h2>
+                        </div>
+                        <Link key={friend.username} to={`/user/${friend.username}`}>
+                        <button>See Profile</button>
+                        </Link>
+                    </div>
+                ))}
+                </div>
+            </div>
+            <div className='users-thumbnails'>
+                <h1 id='users-h1'>All Users</h1>
                 <div className='user-box'>
                 {/*  return all users and sort them in alphabetical order */}
                 {[...dataUsers.users].sort((a, b) => a.username.localeCompare(b.username)).map((user) => (
